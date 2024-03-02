f8d5190b228d41e530412982358bb16cdfa94075
avengsum <sy710481@gmail.com>
2024-03-02 19:39:30 +0530

    added user routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,46 @@
 import { Hono } from 'hono'
-import blog from './routes/blogRoutes'
-import user from './routes/userRoutes'
 import dotnev from 'dotenv'
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { decode, sign, verify } from 'hono/jwt'
+
 
 dotnev.config();
 
-const app = new Hono()
+ export const prisma = new PrismaClient({
+  datasourceUrl:process.env.DATABASE_URL,
+}).$extends(withAccelerate())
 
-app.route('/blog',blog)
-app.route('/user',user)
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+		JWT_SECRET: string,
+	}
+}>();
+
+
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+	const body = await c.req.json();
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		});
+	
+		const jwt = await sign({id:user.id},c.env.JWT_SECRET)
+    return c.json({jwt})
+	} catch(e) {
+		return c.status(403);
+    return c.json({ error: "error while signing up" });
+
+	}
+})
+
 
 
 export default app
