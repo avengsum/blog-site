d747fb9c79f6ed02e0a2367b04afebd8f543d837
avengsum <sy710481@gmail.com>
2024-03-02 19:13:40 +0530

    added prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono'
 import blog from './routes/blogRoutes'
 import user from './routes/userRoutes'
+import dotnev from 'dotenv'
+
+dotnev.config();
 
 const app = new Hono()
 
